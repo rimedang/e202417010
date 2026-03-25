669e2783eddbfa5740e20624c127eac4fd5114da
rimedang <arim0930@gmail.com>
2026-03-25 17:10:41 +0900

    fifth

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
 public static void main(String[] args) {
 System.out.println("이아림");
 World.run();
+World.run2();
 
 }
 }
--- World.java
+++ World.java
@@ -1,5 +1,9 @@
 public class World {
 public static void run() {
 System.out.println("202417010");
+
+}
+public static void run2() {
+System.out.println("hello world");
 }
 }
